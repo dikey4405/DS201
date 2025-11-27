60ae850d3e990d4c2360724f36bef975fe3b7ced
dikey4405 <167569079+dikey4405@users.noreply.github.com>
2025-11-28 01:26:41 +0700

    Update Cider_reward.py

--- DataLoader/Cider_reward.py
+++ DataLoader/Cider_reward.py
@@ -31,46 +31,40 @@ class CIDErReward:
     def compute(self, sampled_seqs, gt_captions_list, beam_size=5):
         """
         Tính toán phần thưởng CIDEr.
-        sampled_seqs: (B * k, T) - Tensor chỉ số từ .sample()
-        gt_captions_list: (B,) - List các list string (ground truth)
         """
         batch_size = len(gt_captions_list)
         
         # 1. Decode các chuỗi đã lấy mẫu (sampled)
         decoded_sampled = self._decode_batch(sampled_seqs) # (B * k) strings
         
-        # 2. Định dạng Ground Truth (GT) - CẦN SỬA Ở ĐÂY
-        # Thư viện yêu cầu định dạng: {img_id: [{'caption': '...'}, ...]}
+        # 2. Định dạng Ground Truth (GT)
         gts = {}
         for i in range(batch_size):
-            # Lỗi cũ: gts[str(i)] = gt_captions_list[i] (Sai vì là list string)
-            # Code mới: Bọc mỗi caption vào dict
+            # Định dạng đúng: {img_id: [{'caption': '...'}, ...]}
             gts[str(i)] = [{'caption': c} for c in gt_captions_list[i]]
             
-        # 3. Định dạng kết quả đã sinh (Res) - CẦN SỬA Ở ĐÂY
-        # Thư viện yêu cầu định dạng: {img_id: [{'caption': '...'}]}
+        # 3. Định dạng kết quả đã sinh (Res)
         res = {}
         for i in range(len(decoded_sampled)):
-            img_id_key = str(i // beam_size) # ID của ảnh (0, 1, 2...)
+            img_id_key = str(i // beam_size) 
             caption = decoded_sampled[i]
             
             if img_id_key not in res:
                 res[img_id_key] = []
             
-            # Lỗi cũ: res[img_id_key].append(caption)
-            # Code mới: Bọc caption vào dict
+            # Định dạng đúng: {img_id: [{'caption': '...'}]}
             res[img_id_key].append({'caption': caption}) 
 
-        # Tokenize (Cần thiết cho CIDEr)
-        # Lúc này gts và res đã đúng chuẩn list of dicts mà PTBTokenizer yêu cầu
+        # Tokenize 
         gts_tokenized = self.tokenizer.tokenize(gts)
         res_tokenized = self.tokenizer.tokenize(res)
         
-        # 4. Tính điểm CIDEr
-        scores, _ = self.cider_scorer.compute_score(gts_tokenized, res_tokenized)
+        # 4. Tính điểm CIDEr - SỬA LỖI Ở ĐÂY
+        # compute_score trả về (average_score, scores_array)
+        # Chúng ta cần scores_array (mảng điểm cho từng ảnh) chứ không phải điểm trung bình
+        _, scores = self.cider_scorer.compute_score(gts_tokenized, res_tokenized)
         
         # 5. Định dạng lại điểm số
-        # scores trả về là numpy array (B * k,)
-        # Cần chuyển về tensor (B, k)
+        # scores bây giờ là mảng có kích thước (batch_size * beam_size)
         rewards = torch.tensor(scores, dtype=torch.float32).view(batch_size, beam_size).to(self.device)
         return rewards
